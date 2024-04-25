ccdf4d8533f0a1236b6e59fefd06c2c19a5575ef
Denys <Eidenn005@gmail.com>
2024-04-25 13:26:23 +0300

    Added BookRepository with implementation Added BookService with implementation Added Book model Added needed files

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,33 @@
 package com.example.onlinebookstore;
 
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineBookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book dune = new Book();
+            dune.setTitle("Wizzard rules");
+            dune.setAuthor("Terry Pratchett");
+            dune.setIsbn("147258369");
+            dune.setPrice(BigDecimal.valueOf(500));
+            bookService.save(dune);
+            System.out.println(bookService.findAll());
+        };
+    }
 }

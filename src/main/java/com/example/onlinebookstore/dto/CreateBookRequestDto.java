c4ded85316f72bf798d693323860d99f3167da4e
Denys <Eidenn005@gmail.com>
2024-05-02 16:21:41 +0300

    GlobalException + validation Added CustomGlobalExceptionHandler Added validation for DTO

--- src/main/java/com/example/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,22 @@
 package com.example.onlinebookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @NotNull
+    @Min(5)
     private String author;
+    @NotNull
+    @Min(13)
     private String isbn;
     private String description;
     private String coverImage;

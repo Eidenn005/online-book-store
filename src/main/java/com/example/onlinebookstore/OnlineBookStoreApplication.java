6e00112c3b42e0a7f29b0076391efcd59d16b97e
Denys Diuimov <156372268+Eidenn005@users.noreply.github.com>
2024-05-02 05:22:49 +0300

    Implement DTO and Mappers (#2)
    * Add BookDto and CreateBookRequestDto
    Add MupStruct mappers
    Add BookController
    Add exception
    Change BookRepository and BookService code
    
    * Minor changes
    
    * Apply suggested changes
    
    * Deleted unused imports

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,9 @@
 package com.example.onlinebookstore;
 
-import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.service.BookService;
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
@@ -16,18 +12,5 @@ public class OnlineBookStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book wizzardRules = new Book();
-            wizzardRules.setTitle("Wizzard rules");
-            wizzardRules.setAuthor("Terry Pratchett");
-            wizzardRules.setIsbn("147258369");
-            wizzardRules.setPrice(BigDecimal.valueOf(500));
-            bookService.save(wizzardRules);
-            System.out.println(bookService.findAll());
-        };
     }
 }

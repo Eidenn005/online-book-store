6e0d715fa35f9272eba9d156499f8fceffe95236
Denys <Eidenn005@gmail.com>
2024-05-14 22:03:24 +0300

    minor changes

--- src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
@@ -27,7 +27,7 @@ public class UserServiceImpl implements UserService {
             UserRegistrationRequestDto requestDto
     ) throws RegistrationException {
         if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new RegistrationException("User already registered");
+            throw new RegistrationException("User with this email is already registered");
         }
         User user = userMapper.toModel(requestDto);
         user.setPassword(passwordEncoder.encode(requestDto.getPassword()));

daaa3e7bd948a5a46ba7c984e9bac3a1e20caad4
Denys <Eidenn005@gmail.com>
2024-05-16 13:01:35 +0300

    Code update

--- src/main/java/com/example/onlinebookstore/config/SecurityConfig.java
+++ src/main/java/com/example/onlinebookstore/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.example.onlinebookstore.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Autowired
     private UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 

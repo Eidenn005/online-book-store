fd4dc6e1bc0acc70c6d6bfc46b7df57e1d8416a7
Denys Diuimov <156372268+Eidenn005@users.noreply.github.com>
2024-05-16 12:17:58 +0300

    Spring Security (#7)
    * Spring Data JPA
    Add SData JPA implementation
    Change BookRepository implementation with SData JPA
    Add and implemented new methods in BookService
    Add LiquiBase dependency
    
    * GlobalException + validation
    Added CustomGlobalExceptionHandler
    Added validation for DTO
    
    * Move hardcode to constants
    
    * Added suggested changes
    
    * Minor checkstyle changes
    
    * Added changes according to review
    
    * fix build
    
    * User and Authentication controller
    User class, DTOs and service added
    AuthenticationController added
    
    * Spring Security
    Added Spring security
    Added Roles and needed classes
    Change User class according to security
    
    * minor changes
    
    * minor changes
    
    * Added changes according to review
    
    * checkstyle changes
    
    * Changes according review

--- src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
@@ -4,15 +4,22 @@ import com.example.onlinebookstore.dto.UserRegistrationRequestDto;
 import com.example.onlinebookstore.dto.UserResponseDto;
 import com.example.onlinebookstore.exception.RegistrationException;
 import com.example.onlinebookstore.mapper.UserMapper;
+import com.example.onlinebookstore.model.Role;
 import com.example.onlinebookstore.model.User;
+import com.example.onlinebookstore.repository.RoleRepository;
 import com.example.onlinebookstore.repository.UserRepository;
 import com.example.onlinebookstore.service.UserService;
+import java.util.Collections;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
@@ -24,6 +31,11 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException("User with this email is already registered");
         }
         User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        Optional<Role> optionalRole = roleRepository.findByRole(Role.RoleName.USER);
+        Role defaultRole = optionalRole.orElseThrow(
+                () -> new IllegalStateException("Default role not found"));
+        user.setRoles(Collections.singleton(defaultRole));
         User savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }

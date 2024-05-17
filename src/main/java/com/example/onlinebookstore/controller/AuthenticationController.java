2918fbc34a27beafe3c5a59184074bd7f96beb9a
Denys Diuimov <156372268+Eidenn005@users.noreply.github.com>
2024-05-17 10:22:33 +0300

    JWT token (#8)
    * Spring Data JPA
    Add SData JPA implementation
    Change BookRepository implementation with SData JPA
    Add and implemented new methods in BookService
    Add LiquiBase dependency
    
    * GlobalException + validation
    Added CustomGlobalExceptionHandler
    Added validation for DTO
    
    * Move hardcode to constants
    
    * Added suggested changes
    
    * Minor checkstyle changes
    
    * Added changes according to review
    
    * fix build
    
    * User and Authentication controller
    User class, DTOs and service added
    AuthenticationController added
    
    * Spring Security
    Added Spring security
    Added Roles and needed classes
    Change User class according to security
    
    * minor changes
    
    * minor changes
    
    * Added changes according to review
    
    * checkstyle changes
    
    * JWT token
    Added JWT token support
    Added UserLoginRequest and UserLoginResponse dtos
    Added JwtUtil and Auth service
    
    * Changes according review
    
    * Code update
    
    * Added changes according to review

--- src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.onlinebookstore.controller;
 
+import com.example.onlinebookstore.dto.UserLoginRequestDto;
+import com.example.onlinebookstore.dto.UserLoginResponseDto;
 import com.example.onlinebookstore.dto.UserRegistrationRequestDto;
 import com.example.onlinebookstore.dto.UserResponseDto;
 import com.example.onlinebookstore.exception.RegistrationException;
+import com.example.onlinebookstore.security.AuthenticationService;
 import com.example.onlinebookstore.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto register(
             @Valid @RequestBody UserRegistrationRequestDto request
     ) throws RegistrationException {
         return userService.save(request);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }

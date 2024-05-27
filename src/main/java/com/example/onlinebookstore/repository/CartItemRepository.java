877bc70895031a0a6b7bb5e8f7ecb9bb14728458
Denys <Eidenn005@gmail.com>
2024-05-27 18:16:43 +0300

    Changes according mentor suggestions

--- src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
@@ -1,7 +1,6 @@
 package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.CartItem;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {

9016f7b5736cc7253cfe1c6e989a026c9ed02387
Denys <Eidenn005@gmail.com>
2024-05-28 17:12:43 +0300

    minor changes

--- src/main/java/com/example/onlinebookstore/repository/ShoppingCartRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/ShoppingCartRepository.java
@@ -5,8 +5,8 @@ import com.example.onlinebookstore.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
 
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
     @EntityGraph(attributePaths = "cartItems.book")
     Optional<ShoppingCart> findByUser(User user);
 }

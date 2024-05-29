38963f5bc8de52f9dbfa2ae356eca5d56254f895
Denys <Eidenn005@gmail.com>
2024-05-29 11:28:54 +0300

    Changes according review

--- src/main/java/com/example/onlinebookstore/model/CartItem.java
+++ src/main/java/com/example/onlinebookstore/model/CartItem.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString(exclude = {"shoppingCart", "book"})
-@EqualsAndHashCode(of = {"id"})
+@EqualsAndHashCode(of = {"id", "quantity"})
 @Table(name = "cart_items")
 public class CartItem {
     @Id

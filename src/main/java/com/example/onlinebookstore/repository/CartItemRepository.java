6125e5c4fac40b32658dd6f5a344745ce86f454e
Denys <Eidenn005@gmail.com>
2024-06-07 19:17:18 +0300

    checkstyle changes

--- src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
@@ -1,6 +1,7 @@
 package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.CartItem;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {

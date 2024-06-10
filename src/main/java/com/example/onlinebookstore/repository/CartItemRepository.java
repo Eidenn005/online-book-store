fc468a3dc21890892ec660cddbf1b548fe95658a
Denys <Eidenn005@gmail.com>
2024-06-10 10:44:30 +0300

    Add changes according to review

--- src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
@@ -1,11 +1,8 @@
 package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.CartItem;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    @EntityGraph(value = "book.id")
-    CartItem findCartItemByBookId(Long id);
 
 }
--- src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
@@ -39,6 +39,7 @@ public class UserServiceImpl implements UserService {
                 () -> new IllegalStateException("Default role not found"));
         user.setRoles(Collections.singleton(defaultRole));
         User savedUser = userRepository.save(user);
+        shoppingCartService.createForUser(savedUser);
         return userMapper.toDto(savedUser);
     }
 }

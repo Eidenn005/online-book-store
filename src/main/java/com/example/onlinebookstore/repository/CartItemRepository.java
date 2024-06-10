1fad024feb75cf9793e913de187c5f2066617954
Denys Diuimov <156372268+Eidenn005@users.noreply.github.com>
2024-06-10 15:24:58 +0300

    Docker dev (#13)
    * Category Model
    Added Category and all needed classes
    Add changes to Book classes
    
    * Added liquibase
    
    * minor changes
    
    * Added changes according to review
    
    * Shopping cart v2
    
    * Shopping cart v2
    Checkstyle minor changes
    
    * Remove annotation
    
    * Mentor commit
    
    * Changes according mentor suggestions
    
    * Changes according mentor suggestions
    
    * Changes according mentor suggestions
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * Order dev
    Add Order entity and all needed classes
    Implement Order and OrderItems services
    
    * Add changes according to review
    
    * checkstyle changes
    
    * checkstyle changes
    
    * Shopping cart v2
    
    * Mentor commit
    
    * Changes according mentor suggestions
    
    * Changes according mentor suggestions
    
    * Changes according mentor suggestions
    
    * minor changes
    
    * Add Docker support
    Added Dockerfile and docker-compose
    Added dependency
    
    * checkstyle changes
    
    * Add changes according to review
    
    ---------
    
    Co-authored-by: AndrewGo0 <goenko4303@gmail.com>

--- src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/CartItemRepository.java
@@ -4,6 +4,5 @@ import com.example.onlinebookstore.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    CartItem findCartItemByBookId(Long id);
 
 }
